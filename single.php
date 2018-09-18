2e7f2341eadfd5904ea918ce987f1bcf46b58414
Matheus Borges <matheus.conquer@gmail.com>
2018-09-18 18:04:50 +0000

    Matheus

--- single.php
+++ single.php
@@ -8,7 +8,7 @@ if (have_posts()) {
 <div class="row">
 	<div class = "col-md-12">
 	<div class="jumbotron">
-	<h2><?php  the_title(); ?></h2>
+	<h1><?php  the_title(); ?></h1>
 		</div>
 	</div>
 </div>
